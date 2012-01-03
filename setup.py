daadc9cdef9ae463b47d6adcd61dcd3818128e73
Xitong Gao <gxtfmx@gmail.com>
2012-01-03 15:54:08 +0000

    Add distribution code

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='akpytemp',
+        version='0.9',
+        description='A simple yet awesome templating utility',
+        maintainer='Xitong Gao',
+        maintainer_email='gxtfmx@gmail.com',
+        url='https://github.com/admk/akpytemp/',
+        package_dir={'akpytemp': ''},
+        packages=['akpytemp'],
+)
