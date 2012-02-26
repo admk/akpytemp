db503c5a93ecf910589c9e265ca18a92387449a3
Xitong Gao <gxtfmx@gmail.com>
2012-02-26 22:49:48 +0000

    Never use colors if not in a color terminal

--- colors.py
+++ colors.py
@@ -1,7 +1,25 @@
-class Colors:
-    HEADER  = '\033[95m'
-    BLUE  = '\033[94m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL    = '\033[91m'
-    END     = '\033[0m'
+import os
+
+__color_enable = 1
+
+def set_color_enable(enable):
+    global __color_enable
+    __color_enable = enable
+
+def check_enable(color):
+    global __color_enable
+    if ('color' in os.environ['TERM']) and __color_enable:
+        return color
+    else:
+        return ''
+
+class Colors(object):
+    """A class with terminal colors
+    This class defines a set of colors suitable for making colored outputs
+    """
+    HEADER = check_enable('\033[95m')
+    BLUE = check_enable('\033[94m')
+    GREEN = check_enable('\033[92m')
+    WARNING = check_enable('\033[93m')
+    FAIL = check_enable('\033[91m')
+    END = check_enable('\033[0m')
